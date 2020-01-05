a671d59f5cf7f30dd38e7950342a5e61c1763295
gruoner <torsten@gruoner.ch>
2020-01-05 16:09:24 +0100

    debug script

---
+++ debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+typeset PUB=/usr/lib/dart/bin/pub
+export https_proxy="http://10.230.1.1:8080"
+echo "======================"
+echo "refreshing build stack"
+echo "======================"
+$PUB get
+$PUB global activate webdev
+echo 
+echo "======================"
+echo "running debug"
+echo "======================"
+$PUB global run webdev serve web:3001
+
+echo "======================"
+echo "FINISHED"
+echo "======================"
+
